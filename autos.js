bc3d7deecc5f510e2bbcf6ec3b520b0ae33ad38b
Linguiniprogramacion <163914530+Linguiniprogramacion@users.noreply.github.com>
2024-07-12 01:23:11 -0400

    Update autos.js

--- autos.js
+++ autos.js
@@ -205,6 +205,88 @@ const autos=[
         "Precio":"30000"
 
     }
+    ,
+    {
+        "id": 20,
+        "nombre": "alfa romeo Giulia Quadrifoglio 2022",
+        "modelo": "sedán deportivo",
+        "pais": "italia",
+        "imagen": "alfa-romeo-giulia.jpg",
+        "precio": "60000"
+    },
+    {
+        "id": 21,
+        "nombre": "porsche Taycan Turbo S 2023",
+        "modelo": "sedán eléctrico",
+        "pais": "alemania",
+        "imagen": "porsche-taycan.jpg",
+        "precio": "150000"
+    },
+    {
+        "id": 22,
+        "nombre": "mercedes-amg GT 63 S 2021",
+        "modelo": "coupé deportivo",
+        "pais": "alemania",
+        "imagen": "mercedes-amg-gt.jpg",
+        "precio": "180000"
+    },
+    {
+        "id": 23,
+        "nombre": "audi RS7 2020",
+        "modelo": "sedán deportivo",
+        "pais": "alemania",
+        "imagen": "audi-rs7.jpg",
+        "precio": "120000"
+    },
+    {
+        "id": 24,
+        "nombre": "bmw i8 Roadster 2019",
+        "modelo": "descapotable deportivo",
+        "pais": "alemania",
+        "imagen": "bmw-i8.jpg",
+        "precio": "100000"
+    },
+    {
+        "id": 25,
+        "nombre": "lexus LC 500 2021",
+        "modelo": "coupé deportivo",
+        "pais": "japón",
+        "imagen": "lexus-lc500.jpg",
+        "precio": "95000"
+    },
+    {
+        "id": 26,
+        "nombre": "aston martin Vantage 2022",
+        "modelo": "coupé deportivo",
+        "pais": "reino unido",
+        "imagen": "aston-martin-vantage.jpg",
+        "precio": "160000"
+    },
+    {
+        "id": 27,
+        "nombre": "ford GT 2020",
+        "modelo": "supercarro",
+        "pais": "estados unidos",
+        "imagen": "ford-gt.jpg",
+        "precio": "500000"
+    },
+    {
+        "id": 28,
+        "nombre": "koenigsegg Jesko 2023",
+        "modelo": "hiperauto",
+        "pais": "suecia",
+        "imagen": "koenigsegg-jesko.jpg",
+        "precio": "3000000"
+    },
+    {
+        "id": 29,
+        "nombre": "bugatti Chiron Super Sport 300+ 2021",
+        "modelo": "hiperauto",
+        "pais": "francia",
+        "imagen": "bugatti-chiron.jpg",
+        "precio": "5000000"
+    }
+
 ];
 
 module.exports={
