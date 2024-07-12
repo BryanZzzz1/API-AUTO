6d2d4bfcaeb8ba6d73b7a7fb9bdab3631821dfba
Linguiniprogramacion <163914530+Linguiniprogramacion@users.noreply.github.com>
2024-07-12 01:47:03 -0400

    Update autos.js

--- autos.js
+++ autos.js
@@ -86,108 +86,9 @@ const autos=[
 
     }
     ,
-    {
-     
-        "id":9,
-        "nombre": "bmw",
-        "modelo": "deportivo",
-        "pais": "alemania",
-        "imagen": "bmw.jpg",
-        "precio":"20000"
-
-    }
-    ,
-    {
-     
-        "id":10,
-        "nombre": "bmw",
-        "modelo": "deportivo",
-        "pais": "alemania",
-        "imagen": "bmw.jpg",
-        "precio":"20000"
-
-    }
-    ,
-    {
-     
-        "id":11,
-        "nombre": "bmw",
-        "modelo": "deportivo",
-        "pais": "alemania",
-        "imagen": "bmw.jpg",
-        "precio":"20000"
-
-    }
-    ,
-    {
-     
-        "id":12,
-        "nombre": "bmw",
-        "modelo": "deportivo",
-        "pais": "alemania",
-        "imagen": "bmw.jpg",
-        "precio":"20000"
-
-    }
-    ,
-    {
-     
-        "id":13,
-        "nombre": "bmw",
-        "modelo": "deportivo",
-        "pais": "alemania",
-        "imagen": "bmw.jpg",
-        "precio":"20000"
-
-    }
-    ,
-    {
-     
-        "id":14,
-        "nombre": "bmw",
-        "modelo": "deportivo",
-        "pais": "alemania",
-        "imagen": "bmw.jpg",
-        "precio":"20000"
-
-    }
-    ,
-    {
-     
-        "id":15,
-        "nombre": "bmw",
-        "modelo": "deportivo",
-        "pais": "alemania",
-        "imagen": "bmw.jpg",
-        "precio":"20000"
-
-    }
-    ,
-    {
-     
-        "id":16,
-        "nombre": "bmw",
-        "modelo": "deportivo",
-        "pais": "alemania",
-        "imagen": "bmw.jpg",
-        "precio":"20000"
-
-    }
-    ,
-    {
-     
-        "id":17,
-        "nombre": "bmw",
-        "modelo": "deportivo",
-        "pais": "alemania",
-        "imagen": "bmw.jpg",
-        "precio":"20000"
-
-    }
-    ,
      {
      
-        "id":18,
+        "id":9,
         "nombre": "Mercedez AMG",
         "modelo": "deportivo",
         "pais": "Peru",
@@ -197,7 +98,7 @@ const autos=[
     ,
      {
      
-        "id":19,
+        "id":10,
         "nombre": "G500",
         "modelo": "Camioneta",
         "pais": "Bolivia",
@@ -207,39 +108,39 @@ const autos=[
     }
     ,
     {
-        "id": 20,
+        "id": 11,
         "nombre": "alfa romeo Giulia Quadrifoglio 2022",
         "modelo": "sedán deportivo",
         "pais": "italia",
-        "imagen": "alfa-romeo-giulia.jpg",
+        "imagen": "alfaromeo.jpg",
         "precio": "60000"
     },
     {
-        "id": 21,
+        "id": 12,
         "nombre": "porsche Taycan Turbo S 2023",
         "modelo": "sedán eléctrico",
         "pais": "alemania",
-        "imagen": "porsche-taycan.jpg",
+        "imagen": "20.jpg",
         "precio": "150000"
     },
     {
-        "id": 22,
+        "id": 13,
         "nombre": "mercedes-amg GT 63 S 2021",
         "modelo": "coupé deportivo",
         "pais": "alemania",
-        "imagen": "mercedes-amg-gt.jpg",
+        "imagen": "ford.jpg",
         "precio": "180000"
     },
     {
-        "id": 23,
+        "id": 14,
         "nombre": "audi RS7 2020",
         "modelo": "sedán deportivo",
         "pais": "alemania",
-        "imagen": "audi-rs7.jpg",
+        "imagen": "30.jpg",
         "precio": "120000"
     },
     {
-        "id": 24,
+        "id": 15,
         "nombre": "bmw i8 Roadster 2019",
         "modelo": "descapotable deportivo",
         "pais": "alemania",
@@ -247,43 +148,43 @@ const autos=[
         "precio": "100000"
     },
     {
-        "id": 25,
+        "id": 16,
         "nombre": "lexus LC 500 2021",
         "modelo": "coupé deportivo",
         "pais": "japón",
-        "imagen": "lexus-lc500.jpg",
+        "imagen": "amg.jpg",
         "precio": "95000"
     },
     {
-        "id": 26,
+        "id": 17,
         "nombre": "aston martin Vantage 2022",
         "modelo": "coupé deportivo",
         "pais": "reino unido",
-        "imagen": "aston-martin-vantage.jpg",
+        "imagen": "30.jpg",
         "precio": "160000"
     },
     {
-        "id": 27,
+        "id": 18,
         "nombre": "ford GT 2020",
         "modelo": "supercarro",
         "pais": "estados unidos",
-        "imagen": "ford-gt.jpg",
+        "imagen": "ford.jpg",
         "precio": "500000"
     },
     {
-        "id": 28,
+        "id": 19,
         "nombre": "koenigsegg Jesko 2023",
         "modelo": "hiperauto",
         "pais": "suecia",
-        "imagen": "koenigsegg-jesko.jpg",
+        "imagen": "camaro.jpg",
         "precio": "3000000"
     },
     {
-        "id": 29,
+        "id": 20,
         "nombre": "bugatti Chiron Super Sport 300+ 2021",
         "modelo": "hiperauto",
         "pais": "francia",
-        "imagen": "bugatti-chiron.jpg",
+        "imagen": "bugati.jpg",
         "precio": "5000000"
     }
 
