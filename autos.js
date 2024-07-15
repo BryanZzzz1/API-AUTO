326040f573a2739304666cdee0dbb5b6c020721c
Linguiniprogramacion <163914530+Linguiniprogramacion@users.noreply.github.com>
2024-07-14 20:03:43 -0400

    Update autos.js

--- autos.js
+++ autos.js
@@ -17,7 +17,7 @@ const autos=[
         "nombre": "Hyundai Santa FE 2018",
         "modelo": "Station Wagon",
         "pais": "japon",
-        "imagen": "hyundai santafe.jpg",
+        "imagen": "hyundai santafe.jpeg",
         "precio":"20000"
 
     },
